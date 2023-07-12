48abbbf49151857478350f7d4c26b6d7499b7189
N3ttX <radko27@centrum.sk>
2023-07-12 18:33:28 +0200

    optional verbosity
    and a tad better error handling

--- global_funcs.py
+++ global_funcs.py
@@ -8,18 +8,28 @@ env_vars = dotenv_values(".env")
 # p.common.logging.basicConfig(level=p.common.DEBUG)
 
 # wrapper for client.exec_command fnc, handles stdout, error logging and paramiko exceptions
-def exec_on_client(client, command, logError=False):
+def exec_on_client(client, command, logError=False, verbose=True):
     try:
+        # execute the command on the client and get stdin, stdout and stderr
         stdin, stdout, stderr = client.exec_command(command)
-        print("stdout on '{}':".format(command))
-        print(stdout.read().decode("ascii"))
+        # format the stdout so it's nice and displayable
+        stdoutF = stdout.read().decode("ascii")
 
+        # only display errors if explicitly set on (Off by default)
         if(logError == True):
             print(stderr.read().decode("ascii").strip("\n"))
+        
+        # only print stdout if set on (On by default)
+        if(verbose):
+            print(stdoutF)
 
+        # return formatted stdout so we can use it
+        return stdoutF
+
+    # if something goes fucky, return the exception
     except p.SSHException as e:
         print(e)
-        return 1
+        return e
     
 # return a connection object type thing, which is then used as parameter for other SSH oriented stuff
 def connect_to_router():
